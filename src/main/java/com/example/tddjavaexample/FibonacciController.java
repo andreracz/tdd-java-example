a26c992718c95809056b0f4ed7082324e27d766a
Andre Racz <andre.racz@avanade.com>
2020-02-07 08:50:54 -0300

    Código para primeiro caso escrito, todos os testes passam

---
+++ src/main/java/com/example/tddjavaexample/FibonacciController.java
@@ -0,0 +1,11 @@
+package com.example.tddjavaexample;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class FibonacciController {
+
+	public int fibonacci(int n) {
+		return 0;
+	}
+}
