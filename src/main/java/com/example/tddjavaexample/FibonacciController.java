9187396c22d24ba591b4b85f88ae5faa5e0a503e
Andre Racz <andre.racz@avanade.com>
2020-02-07 09:10:32 -0300

    Código otimizado, todos os testes passam

--- src/main/java/com/example/tddjavaexample/FibonacciController.java
+++ src/main/java/com/example/tddjavaexample/FibonacciController.java
@@ -6,12 +6,18 @@ import org.springframework.stereotype.Controller;
 public class FibonacciController {
 
 	public int fibonacci(int n) {
+		int a = 0;
+		int b = 1;
+		int c = 0;
 		if (n == 0) {
-			return 0;
+			return a;
 		}
-		if (n == 1) {
-			return 1;
+		for (int i = 2; i <= n; i++) {
+			c = a + b;
+			a = b;
+			b = c;
 		}
-		return fibonacci(n-1) + fibonacci(n-2);
+		return b;
 	}
+	
 }
