1fbc45345ca4963b19461426b9b1188d730fab0a
Andre Racz <andre.racz@avanade.com>
2020-02-07 08:53:16 -0300

    Código para F(1) escrito, todos os testes passam

--- src/main/java/com/example/tddjavaexample/FibonacciController.java
+++ src/main/java/com/example/tddjavaexample/FibonacciController.java
@@ -6,6 +6,12 @@ import org.springframework.stereotype.Controller;
 public class FibonacciController {
 
 	public int fibonacci(int n) {
+		if (n == 0) {
+			return 0;
+		}
+		if (n == 1) {
+			return 1;
+		}
 		return 0;
 	}
 }
