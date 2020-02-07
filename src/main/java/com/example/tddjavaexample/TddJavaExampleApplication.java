43bf13af394033c8af6dcec3f43b9fd7e3d6f547
Andre Racz <andre.racz@avanade.com>
2020-02-07 08:39:38 -0300

    Criação do Projeto

---
+++ src/main/java/com/example/tddjavaexample/TddJavaExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.tddjavaexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TddJavaExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TddJavaExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/tddjavaexample/TddJavaExampleApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.tddjavaexample;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TddJavaExampleApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
